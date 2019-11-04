77d96b5a92c07cafa2b125fbf1ebf93c7dcdaed3
dnrsm <daikinarushima@gmail.com>
2019-11-05 02:19:50 +0900

    add FCM

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from "vue";
+import * as firebase from "firebase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey(
+  ""
+);
+
+messaging
+  .requestPermission()
+  .then(() => {
+    console.log("Notification permission granted.");
+
+    messaging.getToken().then(token => {
+      console.log(token);
+    });
+  })
+  .catch(err => {
+    console.log("Unable to get permission to notify.", err);
+  });
 
 Vue.config.productionTip = false;
 
